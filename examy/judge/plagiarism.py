87895d22edd9ad08290c4eaa81bdde36cb20683f
Abhyuday Choumal <ABHTUDAYCHOUMAL8@GMAIL.COM>
2023-05-03 14:27:57 +0000

    editor

--- examy/judge/plagiarism.py
+++ examy/judge/plagiarism.py
@@ -15,6 +15,8 @@ def run_checker(contest_id: int, penalty_ratio = 0.8):
         # filename="id",
         ).values().annotate(Max('final_score')).order_by('participant_id', 'problem_id')
     
+    if len(submissions) < 2:
+        return
     
     pd.DataFrame.from_dict({
         "filename":[i['submission_file'] for i in submissions],
@@ -22,7 +24,12 @@ def run_checker(contest_id: int, penalty_ratio = 0.8):
         "labels":[i["problem_id"] for i in submissions],
         }).to_csv("./temp_file.csv", index=False)
     
-    output = str(subprocess.check_output("dolos run -f csv -l char temp_file.csv".split(" ")))
+    
+    try:
+        output = str(subprocess.check_output("dolos run -f csv -l char temp_file.csv".split(" ")))
+    except Exception as e:
+        print(e)
+        return
     output_dir = output.split("\\n")[0].split(": ")[1]
     
     similarity = pd.read_csv(f"{output_dir}/pairs.csv")
@@ -32,15 +39,14 @@ def run_checker(contest_id: int, penalty_ratio = 0.8):
         similarity["rightFilePath"] = (similarity["rightFilePath"].apply(lambda x: x.split("_")[1].split(".")[0]))
         similarity["leftFilePath"] = similarity = (similarity["leftFilePath"].apply(lambda x: x.split("_")[1].split(".")[0]))
         
-    try: 
-    
+    try:
         final_set = similarity["leftFilePath"].unique() + similarity["rightFilePath"].unique()
         final_set = set(final_set)
-       
         print(final_set)
         print(similarity["leftFilePath"])
-    except Exception as err: 
+    except Exception as err:
         print(err)
+        return
    
     shutil.rmtree(output_dir, ignore_errors=True)
     os.remove("temp_file.csv")
